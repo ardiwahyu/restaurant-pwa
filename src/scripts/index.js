bc7626c2a9011f09277610fe226d51f16db0d290
Wahyu Ardiansyah <wahyuardi027@gmail.com>
2021-05-21 10:26:11 +0700

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,32 @@
+import 'regenerator-runtime';
+import '../styles/main.css';
+import data from '../DATA.json';
+
+const humberger = document.getElementById('hamburger');
+const nav = document.getElementById('nav');
+humberger.addEventListener("click", function () {
+    if (nav.classList.contains('hidden')) {
+        nav.classList.remove('hidden');
+    } else {
+        nav.classList.add('hidden');
+    }
+})
+
+const row = document.getElementById('container-list');
+data.restaurants.forEach((restaurant) => {
+    console.log(restaurant.name);
+    row.innerHTML = row.innerHTML + `
+        <div class="column">
+            <div class="card" tabindex="0">
+                <img class="icon" alt="${restaurant.name}" src="${restaurant.pictureId}">
+                <div class="city">
+                    <p class="text-city">${restaurant.city}</p>
+                </div>
+                <p class="rating">Rating: ${restaurant.rating}</p>
+                <p class="title">${restaurant.name}</p>
+                <p class="subtitle">${restaurant.description}.</p>
+            </div>
+        </div>
+    `;
+});
+
