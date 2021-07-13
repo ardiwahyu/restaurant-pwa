24d7c68fa27f3179dcedf0cc154c519eb13c804a
Wahyu Ardiansyah <wahyuardi027@gmail.com>
2021-07-13 12:08:02 +0700

    deploy

--- src/scripts/utils/string-from-list.js
+++
@@ -1,9 +0,0 @@
-const extractStringFromList = (arrayObject) => {
-  const listString = [];
-  arrayObject.forEach((element) => {
-    listString.push(element.name);
-  });
-  return listString;
-};
-
-export default extractStringFromList;
